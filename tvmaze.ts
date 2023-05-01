b525308facec2d1b3eb83fae76c3ddf421ab218e
Thomas Wilson <tommyhwilson3@gmail.com>
2023-05-01 14:14:13 -0500

    added search functionality

--- tvmaze.ts
+++ tvmaze.ts
@@ -5,6 +5,15 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const BASE_URL = "https://api.tvmaze.com/search/shows?q="
+
+interface ShowInterface {
+  id: number,
+  name: string,
+  summary: string,
+  image: string
+}
+
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,32 +22,46 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
-}
+async function getShowsByTerm(term: string) : Promise<ShowInterface[]> {
+
+  const shows = await axios.get(`${BASE_URL}${term}`)
+
+  console.log("shows in getshowsbyterm", shows);
+
+
+  return shows.data.map(show => {
+      return {id: show.show.id,
+      name: show.show.name,
+      summary: show.show.summary,
+      image: show.show.image?.medium}
+  })
+};
+
+
+//   return [
+//     {
+//       id: 1767,
+//       name: "The Bletchley Circle",
+//       summary:
+//         `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+//            women with extraordinary skills that helped to end World War II.</p>
+//          <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+//            normal lives, modestly setting aside the part they played in
+//            producing crucial intelligence, which helped the Allies to victory
+//            and shortened the war. When Susan discovers a hidden code behind an
+//            unsolved murder she is met by skepticism from the police. She
+//            quickly realises she can only begin to crack the murders and bring
+//            the culprit to justice with her former friends.</p>`,
+//       image:
+//           "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+//     }
+//   ]
+// }
 
 
 /** Given list of shows, create markup for each and to DOM */
 
-function populateShows(shows) {
+function populateShows(shows: ShowInterface[]) : void {
   $showsList.empty();
 
   for (let show of shows) {
@@ -46,7 +69,7 @@ function populateShows(shows) {
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              src=${show.image}
               alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
@@ -68,15 +91,19 @@ function populateShows(shows) {
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
-async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
+async function searchForShowAndDisplay() : Promise<void> {
+  const term = $("#searchForm-term").val() as string;
+  console.log("term", term);
+
   const shows = await getShowsByTerm(term);
+  console.log("shows", shows);
+
 
   $episodesArea.hide();
   populateShows(shows);
 }
 
-$searchForm.on("submit", async function (evt) {
+$searchForm.on("submit", async function (evt: JQuery.SubmitEvent) {
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
